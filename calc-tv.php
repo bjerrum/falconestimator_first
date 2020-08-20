429103afb99c7d8a9ffa48d1287f4a3e5c08189b
Jesper Bjerrum <jesperbjerrum@gmail.com>
2020-08-20 16:47:19 -0400

    fees42

--- calc-tv.php
+++ calc-tv.php
@@ -24,9 +24,7 @@
 </div>
 
 <div class="question" id="prod_full_term">
-	<?php
-	FalconEstimator::mipi_performers();
-	?>
+	
 	<h4>Production only or full term (signatory)?</h4>
 	<div class="answer" data-set="prod_full_term" data-value="prod_only" data-follow="tv_performers">Production Only</div>
 	<div class="answer" data-set="prod_full_term" data-value="full_term" data-follow="tv_performers">Full Term (signatory)</div>	
