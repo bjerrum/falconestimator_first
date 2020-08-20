edd48c13ac4f49fe6dbcffdc1468766eacea4760
Jesper Bjerrum <jesperbjerrum@gmail.com>
2020-08-20 18:51:27 -0400

    markettest2

--- calc-tv.php
+++ calc-tv.php
@@ -19,7 +19,7 @@
 
 <div class="question" id="prod_full_term">	
 	<h4>Production only or full term (signatory)?</h4>
-	<div class="answer" data-set="prod_full_term" data-value="prod_only" data-follow="tv_performers">Production Only</div>
+	<div class="answer" data-set="prod_full_term" data-value="prod_only" data-follow="mipi_performers">Production Only</div>
 	<div class="answer" data-set="prod_full_term" data-value="full_term" data-follow="tv_performers">Full Term (signatory)</div>
 </div>
 
