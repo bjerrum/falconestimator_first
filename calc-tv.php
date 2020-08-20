bab1e672312c5cd332757939bc7f5e2e5fcd187c
Jesper Bjerrum <jesperbjerrum@gmail.com>
2020-08-19 21:52:41 -0400

    fees10

--- calc-tv.php
+++ calc-tv.php
@@ -11,7 +11,7 @@
 	];
 ?>
 
-<div class="question" id="made_in_played_inx">
+<div class="question" id="made_in_played_in">
 	<h4>Is this a Made-in/Played-in (local)?</h4>
 	<div class="answer" data-set="made_in_played_in" data-value="1">Yes</div>
 	<div class="answer" data-set="made_in_played_in" data-value="0" data-follow="tv_performers">No</div>
