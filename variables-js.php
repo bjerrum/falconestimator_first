96bee331b5104db83e635c7743abaea6b2eb5f07
Jesper Bjerrum <jesperbjerrum@gmail.com>
2020-08-22 21:16:11 -0400

    mipifile42

--- variables-js.php
+++ variables-js.php
@@ -281,7 +281,10 @@ var costs = {
 				},
 				demo: <?php echo get_option('_fpe_tv_broadcast_demo_on', 503.90) ?>,
 				theatrical: <?php echo get_option('_fpe_tv_broadcast_theatrical_on', 671.70) ?>,
-			 } 
+				made_in_played_in: {
+				    'oncam_prod': <?php echo get_option('_mipi_tv_oncam_prin_prod', 570.00) ?>,
+				    'oncam_full': <?php echo get_option('_mipi_tv_oncam_prin_full', 570.00) ?>,
+				} 
 		  },
 		  principal_off: {
 			 session: <?php echo get_option('_fpe_principal_actor_off_camera', 505.05) ?>,
@@ -367,6 +370,10 @@ var costs = {
 				},
 				demo: <?php echo get_option('_fpe_tv_broadcast_demo_off', 503.90) ?>,
 				theatrical: <?php echo get_option('_fpe_tv_broadcast_theatrical_off', 505.05) ?>,
+				made_in_played_in: {					
+				    'offcam_prod': <?php echo get_option('_mipi_tv_offcam_prin_prod', 428.00) ?>,
+				    'offcam_full': <?php echo get_option('_mipi_tv_offcam_prin_full', 428.00) ?>,
+				}
 			 } 
 		  },
 		  stunt_performer: {session: <?php echo get_option('_fpe_principal_actor_on_camera', 671.7) ?>},
@@ -374,7 +381,11 @@ var costs = {
 		  speciality_act: {session: <?php echo get_option('_fpe_principal_actor_on_camera', 671.7) ?>},
 		  dancer: {session: <?php echo get_option('_fpe_principal_actor_on_camera', 671.7) ?>},
 		  singer: {session: <?php echo get_option('_fpe_principal_actor_off_camera', 505.05) ?>},
-		  extra: {session: <?php echo get_option('_fpe_extra_actor', 366,37) ?>}
+		  extra: {session: <?php echo get_option('_fpe_extra_actor', 366,37) ?>}.
+		  made_in_played_in: {			 
+		      'extra_prod': <?php echo get_option('_mipi_tv_extra_perf_prod', 315.00) ?>,
+		      'extra_full': <?php echo get_option('_mipi_tv_extra_perf_full', 315.00) ?>,
+		  }
 	   },
 	   radio: {
 		  announcer: {
@@ -797,14 +808,6 @@ var costs = {
 		  storecast: {
 			 '3': <?php echo get_option('_fpe_psa_storecast_3', 425.50) ?>,
 			 '6': <?php echo get_option('_fpe_psa_storecast_6', 851.00) ?>,
-		  },
-		  made_in_played_in: {
-			 'oncam_prod': <?php echo get_option('_mipi_tv_oncam_prin_prod', 570.00) ?>,
-			 'oncam_full': <?php echo get_option('_mipi_tv_oncam_prin_full', 570.00) ?>,
-			 'offcam_prod': <?php echo get_option('_mipi_tv_offcam_prin_prod', 428.00) ?>,
-			 'offcam_full': <?php echo get_option('_mipi_tv_offcam_prin_full', 428.00) ?>,
-			 'extra_prod': <?php echo get_option('_mipi_tv_extra_perf_prod', 315.00) ?>,
-			 'extra_full': <?php echo get_option('_mipi_tv_extra_perf_full', 315.00) ?>,
 		  }
 	   }
     }
