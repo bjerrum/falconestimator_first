08700203367099479a71d033617fbd7ff347fe8c
Jesper Bjerrum <jesperbjerrum@gmail.com>
2020-08-22 20:25:45 -0400

    mipifile40

--- variables-js.php
+++ variables-js.php
@@ -799,9 +799,12 @@ var costs = {
 			 '6': <?php echo get_option('_fpe_psa_storecast_6', 851.00) ?>,
 		  },
 		  made_in_played_in: {
-			 'oncam': <?php echo get_option('_mipi_tv_oncam_prin', 570.00) ?>,
-			 'offcam': <?php echo get_option('_mipi_tv_offcam_prin', 428.00) ?>,
-			 'extra': <?php echo get_option('_mipi_tv_extra_perf', 315.00) ?>,
+			 'oncam_prod': <?php echo get_option('_mipi_tv_oncam_prin_prod', 570.00) ?>,
+			 'oncam_full': <?php echo get_option('_mipi_tv_oncam_prin_full', 570.00) ?>,
+			 'offcam_prod': <?php echo get_option('_mipi_tv_offcam_prin_prod', 428.00) ?>,
+			 'offcam_full': <?php echo get_option('_mipi_tv_offcam_prin_full', 428.00) ?>,
+			 'extra_prod': <?php echo get_option('_mipi_tv_extra_perf_prod', 315.00) ?>,
+			 'extra_full': <?php echo get_option('_mipi_tv_extra_perf_full', 315.00) ?>,
 		  }
 	   }
     }
